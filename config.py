4e620d489189041173296d4e3969891b15d01365
Ricardo Ruiz <rjruizfdez@gmail.com>
2017-08-06 14:37:06 +0200

    Minor fix

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-
-# There 3 values are in https://www.instagram.com/developer/clients/manage/ . After you create the app, click Manage
-client_id =	""
-client_secret = "" 
-redirect_uri = ""
+# These 3 values are in https://www.instagram.com/developer/clients/manage/ . After you create the app, click Manage
+client_id=""
+client_secret="" 
+redirect_uri=""
 # If you are not sure how to get this value, execute main.py and it will send you to the url with this code
-code = ""
+code=""
